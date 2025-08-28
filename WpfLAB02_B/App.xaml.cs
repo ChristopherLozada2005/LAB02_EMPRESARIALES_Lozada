d536eeec884ef2e9b44588560d4247d0053f20fd
Alumno <Alumno@LAB1502-05>
2025-08-28 18:26:21 -0500

    Agregar archivos de proyecto.

---
+++ WpfLAB02_B/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfLAB02_B
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
