785c9291a72966a20f1ec8ab7cd2a756a98dc747
ChristopherLozada2005 <christopher.lozada@tecsup.edu.pe>
2025-08-28 18:30:49 -0500

    primer commit

--- WpfLAB02_B/MainWindow.xaml.cs
+++ WpfLAB02_B/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ namespace WpfLAB02_B
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Mi primer comentario
         public MainWindow()
         {
             InitializeComponent();
