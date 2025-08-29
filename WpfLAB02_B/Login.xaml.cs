b5541d99be30daa02042124e90e0472a17366450
ChristopherLozada2005 <christopher.lozada@tecsup.edu.pe>
2025-08-28 19:29:57 -0500

    Segundo commit

---
+++ WpfLAB02_B/Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfLAB02_B
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnIngresar_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string password = txtPassword.Password;
+
+            // Ejemplo de credenciales correctas
+            if (usuario == "chris" && password == "1234")
+            {
+                Menu ventanaMenu = new Menu();
+                ventanaMenu.Show();   // Abre el menú
+                this.Close();         // Cierra el login
+            }
+            else
+            {
+                lblMensaje.Text = "❌ Usuario o contraseña incorrectos";
+                lblMensaje.Foreground = System.Windows.Media.Brushes.Red;
+            }
+        }
+    }
+}
--- WpfLAB02_B/Menu.xaml.cs
+++ WpfLAB02_B/Menu.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfLAB02_B
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica de interacción para Menu.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Menu : Window
     {
-        //Mi primer comentario
-        public MainWindow()
+        public Menu()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Home home = new Home();
-            home.Show();
         }
     }
 }
